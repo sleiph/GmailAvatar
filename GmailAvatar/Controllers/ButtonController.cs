c40690f7858acbc90507cfd5092dc929441bc3c1
Ricardo Gomes <sleiph@gmail.com>
2021-08-05 22:58:30 -0300

    desistindo de tudo e recriando do zero

---
+++ GmailAvatar/Controllers/ButtonController.cs
@@ -0,0 +1,35 @@
+﻿using GmailAvatar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GmailAvatar.Controllers
+{
+    public class ButtonController : Controller
+    {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        Random rnd = new Random();
+
+        // GET: Button
+        public ActionResult Index()
+        {
+            for(int i=0; i<28; i++)
+            {
+                if (rnd.Next(10) > 5)
+                    buttons.Add(new ButtonModel(true));
+                else
+                    buttons.Add(new ButtonModel(false));
+            }
+            return View("Index", buttons);
+        }
+
+        public ActionResult HandleButtonClick(string mine)
+        {
+            int buttonNumber = Int32.Parse(mine);
+            buttons[buttonNumber].State = !buttons[buttonNumber].State;
+            return View("Index", buttons);
+        }
+    }
+}
---
+++ GmailAvatar/Models/PersonagemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GmailAvatar.Models
+{
+    public class PersonagemModel
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+
+    }
+}
